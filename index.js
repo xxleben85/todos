56c55572d17f99dd1a3210dcb767131655df108c
Bennett Marcelo <xxleben85@yahoo.com>
2019-01-23 12:10:28 +0800

    working on js file

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    datatype:  'json',
+    success: function(response, textStatus) {
+      response.tasks.forEach(function(task) {
+        console.log(task.content)
+      })
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+  
+});
