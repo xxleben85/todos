3132afa1cf65b52835f3bdbc270f51aa53891c7e
Bennett Marcelo <xxleben85@yahoo.com>
2019-01-23 16:37:52 +0800

    finishing up

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ $(document).ready(function() {
       url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=68',
       datatype:  'json',
       success: function(response, textStatus) {
+        $('#to-do').empty();
         response.tasks.forEach(function(task) {
-          $('#to-do').append('<p>' + task.content + '</p>');
+          $('#to-do').append('<div class="row row-list py-2"><span class="col-xs-9 todo-item">' + task.content + '</span><button class="btn btn-danger remove mx-4" data-id="' + task.id + '">Remove</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.complete? 'Checked' : '') + '></div>');
         })
       },
       error: function(request, textStatus, errorMessage) {
@@ -28,7 +29,8 @@ $(document).ready(function() {
         }
       }),
       success: function(response, textStatus) {
-        console.log(response);
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
       },
       error: function(request, textStatus, errorMessage) {
         console.log(errorMessage)
@@ -40,6 +42,43 @@ $(document).ready(function() {
     e.preventDefault();
     createTask();
   });
+
+  var deleteItem = function(id) {
+    $.ajax({
+      type:  'DELETE',
+      url:  'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=68',
+      success:  function(response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error:  function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
+    })
+  };
+
+$(document).on('click', '.remove', function() {
+  deleteItem($(this).data('id'));
+})
+
+var markComplete = function(id) {
+  $.ajax({
+    type: 'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=68',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      getAndDisplayAllTasks();
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  })
+};
+
+$(document).on('change', '.mark-complete', function() {
+  if (this.checked) {
+    markComplete($(this).data('id'));
+  }
+})
 
   getAndDisplayAllTasks();
 
