1f9222c96e4b7195481fbcc65b5842915720324f
Bennett Marcelo <xxleben85@yahoo.com>
2019-01-23 13:06:40 +0800

    created Get and Post functions in .js

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 $(document).ready(function() {
 
-  $.ajax({
-    type: 'GET',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
-    datatype:  'json',
-    success: function(response, textStatus) {
-      response.tasks.forEach(function(task) {
-        console.log(task.content)
-      })
-    },
-    error: function(request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
+  var getAndDisplayAllTasks = function() {
+    $.ajax({
+      type: 'GET',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=68',
+      datatype:  'json',
+      success: function(response, textStatus) {
+        response.tasks.forEach(function(task) {
+          $('#to-do').append('<p>' + task.content + '</p>');
+        })
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+  var createTask = function() {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=68',
+      contentType: 'application/json',
+      datatype: 'json',
+      data:  JSON.stringify({
+        task:  {
+          content:  $('#new-task-content').val()
+        }
+      }),
+      success: function(response, textStatus) {
+        console.log(response);
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage)
+      }
+    })
+  };
+
+  $('#create-task').on('submit', function(e) {
+    e.preventDefault();
+    createTask();
   });
-  
+
+  getAndDisplayAllTasks();
+
 });
